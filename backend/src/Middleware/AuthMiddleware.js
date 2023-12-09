a5495a859c516686ea22be9da1260acdce94e6e7
ptrdung <91458242+ptrdung@users.noreply.github.com>
2023-12-09 23:45:03 +0700

    Update1 AuthMiddleware.js

--- backend/src/Middleware/AuthMiddleware.js
+++ backend/src/Middleware/AuthMiddleware.js
@@ -27,3 +27,11 @@ const protect = asyncHandler(async (req, res, next) => {
     throw new Error("Not authorized, no token");
   }
 });
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an Admin");
+  }
+};
