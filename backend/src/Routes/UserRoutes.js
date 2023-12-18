210de2e71dcbccb52236a7a76774ec451a4bea98
nguyenmanhdung128@gmail.com <nguyenmanhdung128@gmail.com>
2023-12-18 23:08:10 +0700

    login register router

---
+++ backend/src/Routes/UserRoutes.js
@@ -0,0 +1,66 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import { protect, admin } from "../Middleware/AuthMiddleware.js";
+import generateToken from "../utils/generateToken.js";
+import User from "./../Models/UserModel.js";
+
+const userRouter = express.Router();
+
+// LOGIN
+userRouter.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid Email or Password");
+    }
+  })
+);
+
+// REGISTER
+userRouter.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid User Data");
+    }
+  })
+);
+
+export default userRouter;
---
+++ backend/src/Routes/orderRoutes.js
@@ -0,0 +1,45 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import { admin, protect } from "../Middleware/AuthMiddleware.js";
+import Order from "./../Models/OrderModel.js";
+
+const orderRouter = express.Router();
+
+// CREATE ORDER
+orderRouter.post(
+  "/",
+  protect,
+  asyncHandler(async (req, res) => {
+    const {
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+      res.status(400);
+      throw new Error("No order items");
+      return;
+    } else {
+      const order = new Order({
+        orderItems,
+        user: req.user._id,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+      });
+
+      const createOrder = await order.save();
+      res.status(201).json(createOrder);
+    }
+  })
+);
+
+export default orderRouter;
