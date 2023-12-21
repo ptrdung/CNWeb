e728529cc89c165b365257799fb2c559f8b569f4
ptrdung <91458242+ptrdung@users.noreply.github.com>
2023-12-21 20:16:12 +0700

    Update Products.js

--- backend/src/data/Products.js
+++ backend/src/data/Products.js
@@ -40,7 +40,7 @@ const products = [
     numReviews: 9,
   },
  {
-    name: "Lace Walking Shoes For Boys & Girls  ",
+    name: "Lace Walking Shoes For Boys & Girls (Pink) ",
     image: "/images/2.png",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
