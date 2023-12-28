3cfe131a2d8ceb8199b84eed565fc6801b3f2f66
ptrdung <91458242+ptrdung@users.noreply.github.com>
2023-12-28 16:10:19 +0700

    Update Products.js

--- backend/src/data/Products.js
+++ backend/src/data/Products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    name: "Velcro Ballerinas For Girls  (Pink)",
+    name: "Velcro Ballerinas For Girls  (Yellow)",
     image: "/images/6.png",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
